96d086c331a14072ecd83fb82d55da51b1c1689b
Chris Frye <chris.frye@gmail.com>
2011-05-08 18:06:29 +0000

    reordered things

--- js/crateslider.js
+++ js/crateslider.js
@@ -1,3 +1,12 @@
+function parseNode(node) { // takes a node object and turns it into a <li>
+    var li = document.createElement("LI");
+    li.innerHTML = node.doc.name;
+    li.className = "category";
+    li.id = node.doc.id;
+    return li;
+}
+
+
 function parseNodes(nodes, idString) { // takes a nodes array and turns it into a <ol>
     var ul = document.createElement("ul");
     ul.id = idString;
@@ -8,13 +17,7 @@ function parseNodes(nodes, idString) { // takes a nodes array and turns it into 
     return ul;
 }
 
-function parseNode(node) { // takes a node object and turns it into a <li>
-    var li = document.createElement("LI");
-    li.innerHTML = node.doc.name;
-    li.className = "category";
-    li.id = node.doc.id;
-    return li;
-}
+
 
 function getRootCategories(){
 	$.getJSON("http://crateandbarrel.couchone.com/category_categories/_all_docs?startkey=%220:0%22&endkey=%220:%5cu9999%22&include_docs=true&&callback=?",
